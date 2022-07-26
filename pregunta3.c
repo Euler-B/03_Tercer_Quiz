84e97dc2c3c1e3178727284129b0d256364d65b2
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-25 23:18:34 -0400

    add functional code on pregunta3

--- pregunta3.c
+++ pregunta3.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    short edad = 21;
 
+    if (edad >= 0 && edad <= 2)
+    printf("Estimado Bebe, Buenos dias \n");
+
+    if (edad >= 3 && edad <= 14)
+    printf("Estimado Niño, Buenos dias  \n");
+
+    if (edad >= 15 && edad <= 20)
+    printf("Estimado Adolescente, Buenos dias  \n");
+
+    if (edad > 20)
+    printf("Estimado Adulto, Buenos dias  \n");
 
 }
