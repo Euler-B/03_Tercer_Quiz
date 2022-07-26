a1d9939b19f10976aea344e5d47aea78bf892379
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-25 22:51:46 -0400

    functional code exercise 2 and thinking a refactor for exercise 1

--- pregunta1.c
+++ pregunta1.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    //short contador;
-    short i;
-
-    for (i = 0; i >= 0 && i <= 20; i++){
-
-        if (i % 2 == 0) {
-        printf("%d \t --->   Es un numero par =) \n", i);
-        } else {
-        continue;
-        }
-    }
+    /*
+        Repensar la solucion de este programa en base a una estructura secuencial,
+        buscar ejemplos en internet, preguntar a compañeros, 
+        ver apuntes de otras clases a la cuales no asisti
+    
+    
+    
+    */
 }
