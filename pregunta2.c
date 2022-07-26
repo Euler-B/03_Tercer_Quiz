d3ee485e6b022f85a9b96e4ad992cd7346c798f1
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-07-25 22:53:26 -0400

    add mising archives

---
+++ pregunta2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    short i;
+
+    for (i = 0; i >= 0 && i <= 20; i++){
+
+        if (i % 2 == 0) {
+        printf("%d \t --->   Es un numero par =) \n", i);
+        } else {
+        continue;
+        }
+    }
+}
